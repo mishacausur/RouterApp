6df7652c15579db443691846b9997b73a54f56ee
Misha Causur <mishacausur@mail.ru>
2022-01-05 22:33:12 +0500

    Initial Commit

---
+++ RealtyEstate/RealtyEstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtyEstateApp.swift
+//  RealtyEstate
+//
+//  Created by Misha Causur on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RealtyEstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
