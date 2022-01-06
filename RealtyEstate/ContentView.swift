d86ebf9ef2434dc5946b970476d9e4c75a617d08
Misha Causur <mishacausur@mail.ru>
2022-01-06 19:50:39 +0500

    first view is ready

--- RealtyEstate/ContentView.swift
+++ RealtyEstate/ContentView.swift
@@ -8,16 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var items = ["1", "2", "3"]
     var body: some View {
-        NavigationView {
-            List(items, id: \.self) { i in
-                Router.eventOccured(with: .toCardView(i)) {
-                    Text(i)
+        VStack {
+            Spacer()
+            VStack {
+                Circle().fill(Color.white)
+                    .frame(width: 104)
+                    .overlay {
+                        Image("boxIcon")
+                    }
+                    .padding(.top, 40)
+                    .padding(.top, 45.0)
+                Text("Non-Contact \nDeliveries")
+                    .font(.title.bold())
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("textPrimary"))
+                    .padding(.top, 24)
+                Text("When placing an order, select the option “Contactless delivery” and the courier will leave your order at the door.")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("textSecondary"))
+                    .padding(.all, 20.0)
+                    
+                Button {
+                    
+                } label: {
+                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                        .fill(Color("primaryButton"))
+                        .frame(height: 56)
+                        .overlay {
+                            Text("Order Now".uppercased())
+                                .font(.footnote.bold())
+                                .foregroundColor(.white)
+                        }
                 }
-                .navigationTitle("YYY")
+                .padding(.bottom, 13)
+                .padding(.horizontal, 10)
+                Button { } label: {
+                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                        .fill(Color("backLight"))
+                        .frame(height: 56)
+                        .overlay {
+                            Text("Dismiss".uppercased())
+                                .foregroundColor(Color("textSecondary"))
+                                .font(.footnote.bold())
+                        }
+                }
+                .padding(.bottom, 24)
+                .padding(.horizontal, 10)
+                
             }
+            .frame(maxWidth: .infinity)
+            .frame(height: 564)
+            .background(
+                RoundedRectangle(cornerRadius: 25, style: .continuous).fill(Color("backLight")))
         }
+        .background(
+            Image("background 1"))
+        .ignoresSafeArea()
     }
 }
 
